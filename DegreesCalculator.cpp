43b3cea9f9eba0cc58712767e2aa3226fa16c9be
Vasilaki Gorgioski <vasilakigorgi@gmail.com>
2022-07-24 13:15:23 +0200

    Add files via upload
    15-degree change calculator for automatization of photogrammetry positions

---
+++ DegreesCalculator.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    for(int i=215, count=0; i>360, count<=12; i+=15, count++){
+        cout<<count<<". "<<i<<endl;
+    }
+
+    return 0;
+}  
