8da408dac27dbed0bef9ce59eb78f942590d8d71
Vasilaki Gorgioski <vasilakigorgi@gmail.com>
2023-02-02 00:08:10 +0100

    Successfull upload, need to buy photoresistor

--- Pendulum/pendulum/src/main.cpp
+++ Pendulum/pendulum/src/main.cpp
@@ -1,20 +1,37 @@
 #include <Arduino.h>
 
-int LaserDetectorPin = 2;
-char DELIM = '\t';
-
+// char DELIM = '\t';
+int laserDetectorPin = 2;
 long baud_rate = 2000000;
 
-bool photoGate;
-bool lastState = 0;
+bool eq_state = false;
+int eq_state_light = 0;
 
-int eventNum = 1;
-long eventTime;
+float timeCounter = 0;
+float prevTime = 0;
+float currTime = 0;
+float period = 0;
+float halfPeriod = 0;
 
-void setup(){
+float hits[11];
+float firstHit = 0;
+int event = 0;
+
+float length = 0;
+
+void clearscreen()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  }
+}
+
+void setup()
+{
   Serial.begin(baud_rate);
 
-  pinMode(LaserDetectorPin, INPUT_PULLUP);
+  pinMode(laserDetectorPin, INPUT); //_PULLUP
   pinMode(13, OUTPUT);
 
   Serial.print("event");
@@ -27,20 +44,92 @@ void setup(){
   Serial.println("======================");
 }
 
-void loop(){
-  photoGate = digitalRead(LaserDetectorPin);
-  digitalWrite(13, !photoGate);
-  
-  if (photoGate != lastState)
-    {
-      eventTime = millis();
-      Serial.print(eventNum);
-      Serial.print(DELIM);
-      Serial.print(eventTime / 1000.0, 3);
-      Serial.print(DELIM);
-      Serial.println(photoGate);
-      eventNum++;
-    }
-  lastState = photoGate;
+void loop()
+{
+  clearscreen();
+  Serial.println("Calibrating the sensor...");
 
+  eq_state_light = digitalRead(laserDetectorPin) - 2;
+  eq_state = true;
+
+  while (true)
+  {
+    if (digitalRead(laserDetectorPin) < eq_state_light)
+    {
+      Serial.println("Ball left the eq state");
+      eq_state = false;
+      break;
+    }
+
+    clearscreen();
+    Serial.println("Color Sensor RAW:" + digitalRead(laserDetectorPin));
+  }
+
+  timeCounter = 0;
+  prevTime = millis() / 1000.0;
+
+  for (int i = 0; i < 10; i++)
+  {
+    hits[i] = 0.0;
+  }
+
+  while (true)
+  {
+    //-- -- -- -- -- - Time Handling -- -- -- -- -- -- -- -
+    currTime = millis() / 1000.0;
+    timeCounter += (currTime - prevTime);
+    prevTime = currTime;
+    //-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+    if (digitalRead(laserDetectorPin) >= eq_state_light)
+    {
+      event = 0;
+      hits[event] = timeCounter;
+      Serial.print("Hit RAW:");
+      Serial.println(timeCounter);
+      delay(500);
+      if (event > 10)
+      {
+        break;
+      }
+    }
+  }
+
+  clearscreen();
+  firstHit = hits[0];
+  Serial.println("-------- DONE --------");
+  Serial.println("Hits shifted by the first hit value...");
+
+  for (int i = 0; i < 10; i++)
+  {
+    Serial.print("Hit ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(hits[i] - firstHit);
+  }
+
+  halfPeriod = (hits[1] - hits[0]);
+
+  for (int i = 0; i < 9; i++)
+  {
+    halfPeriod = (halfPeriod + (hits[i + 1] - hits[i])) / 2.0;
+  }
+
+  period = halfPeriod * 2.0;
+  length = period * period * 9.81 / (4.0 * PI * PI);
+
+  Serial.print("Average Period: ");
+  Serial.print(period);
+  Serial.println("s");
+
+  Serial.print("Length of the pendulum is: ");
+  Serial.print(period);
+  Serial.println("m");
+
+  Serial.print("Length of the pendulum is: ");
+  Serial.print(period * 100.0);
+  Serial.println("cm");
+
+  Serial.println("Restarting...");
+  delay(1000);
 }
